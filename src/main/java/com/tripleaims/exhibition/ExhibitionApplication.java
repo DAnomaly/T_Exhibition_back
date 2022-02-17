062820d309902661933982289a863029ac8af60d
ektha4416 <dlektha4416@naver.com>
2022-02-17 10:48:51 +0900

    dddddd

--- src/main/java/com/tripleaims/exhibition/ExhibitionApplication.java
+++ src/main/java/com/tripleaims/exhibition/ExhibitionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExhibitionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExhibitionApplication.class, args);
+		SpringApplication.run(ExhibitionApplication.class, args); 
 	}
 
 }
