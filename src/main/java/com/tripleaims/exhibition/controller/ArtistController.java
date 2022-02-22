8c06723a83de17ebe7bf4c343252ea715d2ac257
DAnomaly <parksh1219@naver.com>
2022-02-22 16:55:43 +0900

    dto 기능 추가
    '@NoArgsConstructor'를 모든 dto에 추가

---
+++ src/main/java/com/tripleaims/exhibition/controller/ArtistController.java
@@ -0,0 +1,15 @@
+package com.tripleaims.exhibition.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("artist")
+@AllArgsConstructor
+@CrossOrigin("*")
+public class ArtistController {
+
+}
--- src/main/java/com/tripleaims/exhibition/dto/OrderShowDTO.java
+++ src/main/java/com/tripleaims/exhibition/dto/OrderShowDTO.java
@@ -4,12 +4,14 @@ import java.sql.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 public class OrderShowDTO {
 
