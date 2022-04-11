3fc6906af9e5d67ff28eb13bdf3f296e8ee81356
DAnomaly <parksh1219@naver.com>
2022-04-11 14:52:01 +0900

    Edit Config

--- src/main/java/com/tripleaims/exhibition/util/ExhibitionConfig.java
+++ src/main/java/com/tripleaims/exhibition/util/ExhibitionConfig.java
@@ -2,7 +2,10 @@ package com.tripleaims.exhibition.util;
 
 public class ExhibitionConfig {
 
-	public final static String REFERER_URL = "http://192.168.0.65:5500";
+	// URL
+	public final static String REFERER_URL = "http://172.0.0.1:5500";
+	
+	// PATH
 	public final static String PROFILE_PATH = "C:/workspace/staticWeb/exhibition_static/assets/profiles";
 	public final static String FRONT_PATH = "C:/workspace/staticWeb/exhibition_static";
 }
